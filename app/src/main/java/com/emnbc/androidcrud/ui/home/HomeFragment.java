a9e33e3ee85e3fe258ec036bc1d730b51b305556
Evgeny Tupikov <bwinter@yandex.ru>
2021-04-04 22:33:22 +0500

    Button listener and fix toolbar

--- app/src/main/java/com/emnbc/androidcrud/ui/home/HomeFragment.java
+++ app/src/main/java/com/emnbc/androidcrud/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,11 +19,10 @@ import com.emnbc.androidcrud.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button btn;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -30,6 +31,22 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        addListenerOnButton(root);
+
         return root;
     }
+
+    public void addListenerOnButton(View v) {
+        btn = (Button) v.findViewById(R.id.button);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getActivity(),"Hello world!",Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
 }
