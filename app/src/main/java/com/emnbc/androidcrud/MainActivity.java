297197e373f41985dacc1e29caf87e5df6352718
Evgeny Tupikov <bwinter@yandex.ru>
2021-04-06 00:36:08 +0500

    Toolbar settings

--- app/src/main/java/com/emnbc/androidcrud/MainActivity.java
+++ app/src/main/java/com/emnbc/androidcrud/MainActivity.java
@@ -1,14 +1,19 @@
 package com.emnbc.androidcrud;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,28 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                // do your sign-out stuff
+                Toast.makeText(getApplicationContext(),"This is Settings",Toast.LENGTH_LONG).show();
+                break;
+            }
+            // case blocks for other MenuItems (if any)
+        }
+        return true;
     }
 
 }
