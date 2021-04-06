759400faf0fb98ef4f3c666f6c706ee136fac80a
Evgeny Tupikov <bwinter@yandex.ru>
2021-04-06 23:18:10 +0500

    Get Request

--- app/src/main/java/com/emnbc/androidcrud/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/emnbc/androidcrud/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,11 @@
 package com.emnbc.androidcrud.ui.dashboard;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,22 +16,72 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.emnbc.androidcrud.R;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private Button btn;
+    private TextView textView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        addListenerOnButton(root);
         return root;
+    }
+
+    public void addListenerOnButton(View v) {
+        btn = (Button) v.findViewById(R.id.button2);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new AsyncRequest().execute();
+                    }
+                }
+        );
+    }
+
+    class AsyncRequest extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... arg) {
+
+            OkHttpClient httpClient = new OkHttpClient();
+
+            String url = "http://pizzashop.emnbc.com/api/pizzas";
+            Request request = new Request.Builder().url(url).build();
+
+            Response response = null;
+            try {
+                response = httpClient.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                textView.setText("Fuck");
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView.setText(s);
+        }
     }
 }
