290f871e9890a273633c06bfee95630924a78854
Evgeny Tupikov <bwinter@yandex.ru>
2021-04-17 01:19:20 +0500

    Toggle button and fix sending

--- app/src/main/java/com/emnbc/androidcrud/services/RunTimer.java
+++ app/src/main/java/com/emnbc/androidcrud/services/RunTimer.java
@@ -1,11 +1,9 @@
 package com.emnbc.androidcrud.services;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.widget.Toast;
-import java.util.Timer;
 
 public class RunTimer {
 
@@ -25,32 +23,29 @@ public class RunTimer {
         }
     }
 
-    public void  stopSending() {
+    public void stopSending() {
         isActive = false;
+    }
+
+    public Boolean getStatus() {
+        return isActive;
     }
 
     private void sending() {
         mActivity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    new Handler().postDelayed(
-                        new Runnable() {
-                            public void run() {
-                                if (isActive) {
-                                    Toast.makeText(mInflater.getContext(), "Test", Toast.LENGTH_SHORT).show();
+                    if (isActive) {
+                        Toast.makeText(mInflater.getContext(), "Test", Toast.LENGTH_SHORT).show();
+                        new Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
                                     sending();
                                 }
-                            }
-                        }, 3000);
+                            }, 3000);
+                    }
                 }
             }
         );
-//        new Handler().postDelayed(
-//                new Runnable() {
-//                    public void run() {
-//                        Log.i("tag","A Kiss after 5 seconds");
-//                        Run();
-//                    }
-//                }, 1000);
     }
 }
--- app/src/main/java/com/emnbc/androidcrud/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/emnbc/androidcrud/ui/notifications/NotificationsFragment.java
@@ -6,10 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,15 +22,26 @@ public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private RunTimer runTimer;
-    private Button btnStart;
-    private Button btnStop;
+    private Button btn;
+    private String startSnd;
+    private String stopSnd;
+    private int colorGreen;
+    private int colorRed;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
 
+        startSnd = getResources().getString(R.string.sending_state_start);
+        stopSnd = getResources().getString(R.string.sending_state_stop);
+
+        colorGreen = ContextCompat.getColor(getContext(), R.color.color_green);
+        colorRed = ContextCompat.getColor(getContext(), R.color.color_red);
+
         runTimer = ((MainActivity) getActivity()).getRunTimer();
+        btn = (Button) root.findViewById(R.id.buttonToggle);
+        initBtn(btn, runTimer.getStatus());
 
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -44,27 +55,35 @@ public class NotificationsFragment extends Fragment {
     }
 
     public void addListenerOnButtons(View v) {
-        btnStart = (Button) v.findViewById(R.id.buttonStart);
-        btnStop = (Button) v.findViewById(R.id.buttonStop);
 
-        btnStart.setOnClickListener(
+        btn.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        runTimer.startSending();
-                        // Toast.makeText(getActivity(),"Hello world!",Toast.LENGTH_SHORT).show();
+
+                        if (runTimer.getStatus()) {
+                            runTimer.stopSending();
+                            btn.setText(startSnd);
+                            btn.setBackgroundColor(colorGreen);
+                        } else {
+                            runTimer.startSending();
+                            btn.setText(stopSnd);
+                            btn.setBackgroundColor(colorRed);
+                        }
+
                     }
                 }
         );
 
-        btnStop.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        runTimer.stopSending();
-                        // Toast.makeText(getActivity(),"Hello world!",Toast.LENGTH_SHORT).show();
-                    }
-                }
-        );
+    }
+
+    private void initBtn(Button btn, Boolean state) {
+        if (state) {
+            btn.setText(stopSnd);
+            btn.setBackgroundColor(colorRed);
+        } else {
+            btn.setText(startSnd);
+            btn.setBackgroundColor(colorGreen);
+        }
     }
 }
