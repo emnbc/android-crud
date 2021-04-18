1a6a252b39f19445ff2b288a292b6af199b47fac
Evgeny Tupikov <bwinter@yandex.ru>
2021-04-18 22:46:24 +0500

    Fix get and post method

--- app/src/main/java/com/emnbc/androidcrud/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/emnbc/androidcrud/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,5 @@
 package com.emnbc.androidcrud.ui.dashboard;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +9,10 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,14 +23,9 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class DashboardFragment extends Fragment implements ItemViewAdapter.ItemClickListener {
 
-    // private View root;
-    // private Button btn;
+    private DashboardViewModel dashboardViewModel;
     private ArrayList<Item> itemNames;
     private RecyclerView recyclerView;
     private ItemViewAdapter adapter;
@@ -37,13 +34,37 @@ public class DashboardFragment extends Fragment implements ItemViewAdapter.ItemC
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
-        itemNames = new ArrayList<>();
         recyclerView = root.findViewById(R.id.rvItems);
         progressBar = root.findViewById(R.id.progressBar);
+
+        dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        itemNames = new ArrayList<>();
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new ItemViewAdapter(getContext(), itemNames);
         recyclerView.setAdapter(adapter);
+
+        dashboardViewModel.getItems().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+                Gson gson = new Gson();
+                Item[] itemsArray = gson.fromJson(s, Item[].class);
+
+                itemNames.clear();
+
+                for(int i = 0; i < itemsArray.length; i++) {
+                    itemNames.add(itemsArray[i]);
+                }
+
+                adapter = new ItemViewAdapter(getContext(), itemNames);
+                adapter.setClickListener(DashboardFragment.this);
+                recyclerView.setAdapter(adapter);
+
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
 
         addListenerOnButton(root);
         return root;
@@ -51,11 +72,34 @@ public class DashboardFragment extends Fragment implements ItemViewAdapter.ItemC
 
     public void addListenerOnButton(View v) {
         Button btn = (Button) v.findViewById(R.id.button2);
+        Button postBtn = (Button) v.findViewById(R.id.postBtn);
+
         btn.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        new AsyncRequest().execute();
+                        // new AsyncRequest().execute();
+                        try {
+                            progressBar.setVisibility(View.VISIBLE);
+                            dashboardViewModel.getHttpResponse();
+                        } catch (IOException e) {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+
+        // postBtn.setEnabled(false);
+        postBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            dashboardViewModel.postHttpResponse();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
         );
@@ -66,50 +110,50 @@ public class DashboardFragment extends Fragment implements ItemViewAdapter.ItemC
         Toast.makeText(getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
-    class AsyncRequest extends AsyncTask<String, Integer, String> {
-
-        @Override
-        protected void onPreExecute()
-        {
-            progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected String doInBackground(String... arg) {
-
-            OkHttpClient httpClient = new OkHttpClient();
-
-            String url = "http://pizzashop.emnbc.com/api/pizzas";
-            Request request = new Request.Builder().url(url).build();
-
-            Response response = null;
-            try {
-                response = httpClient.newCall(request).execute();
-                return response.body().string();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            Gson gson = new Gson();
-            Item[] itemsArray = gson.fromJson(s, Item[].class);
-
-            itemNames.clear();
-
-            for(int i = 0; i < itemsArray.length; i++) {
-                itemNames.add(itemsArray[i]);
-            }
-
-            adapter = new ItemViewAdapter(getContext(), itemNames);
-            adapter.setClickListener(DashboardFragment.this);
-            recyclerView.setAdapter(adapter);
-
-            progressBar.setVisibility(View.INVISIBLE);
-        }
-    }
+//    class AsyncRequest extends AsyncTask<String, Integer, String> {
+//
+//        @Override
+//        protected void onPreExecute()
+//        {
+//            progressBar.setVisibility(View.VISIBLE);
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... arg) {
+//
+//            OkHttpClient httpClient = new OkHttpClient();
+//
+//            String url = "http://pizzashop.emnbc.com/api/pizzas";
+//            Request request = new Request.Builder().url(url).build();
+//
+//            Response response = null;
+//            try {
+//                response = httpClient.newCall(request).execute();
+//                return response.body().string();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String s) {
+//            super.onPostExecute(s);
+//
+//            Gson gson = new Gson();
+//            Item[] itemsArray = gson.fromJson(s, Item[].class);
+//
+//            itemNames.clear();
+//
+//            for(int i = 0; i < itemsArray.length; i++) {
+//                itemNames.add(itemsArray[i]);
+//            }
+//
+//            adapter = new ItemViewAdapter(getContext(), itemNames);
+//            adapter.setClickListener(DashboardFragment.this);
+//            recyclerView.setAdapter(adapter);
+//
+//            progressBar.setVisibility(View.INVISIBLE);
+//        }
+//    }
 }
--- app/src/main/java/com/emnbc/androidcrud/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/emnbc/androidcrud/ui/dashboard/DashboardViewModel.java
@@ -1,19 +1,88 @@
 package com.emnbc.androidcrud.ui.dashboard;
 
+import android.util.Log;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class DashboardViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<String> mItems;
 
     public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mItems = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getItems() {
+        return mItems;
+    }
+
+
+    public void getHttpResponse() throws IOException {
+
+        String url = "http://pizzashop.emnbc.com/api/pizzas";
+
+        OkHttpClient httpClient = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .build();
+
+        httpClient.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String mMessage = response.body().string();
+                mItems.postValue(mMessage);
+            }
+        });
+    }
+
+    public void postHttpResponse() throws IOException {
+
+        String url = "http://track-me.emnbc.com/api/track/send";
+
+        OkHttpClient httpClient = new OkHttpClient();
+        final MediaType mediaType = MediaType.parse("application/json");
+
+        String jsonStr = "{\"trackId\":\"868239606757\", \"lngX\" : 54.795899, \"latY\" : 55.959599}";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(mediaType, jsonStr))
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .build();
+
+        httpClient.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String mMessage = response.body().string();
+                Log.e("TEST POST", mMessage);
+            }
+        });
     }
 }
